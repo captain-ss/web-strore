c65b1e8b471b1a8da8fede63201ee389a1233eb1
captain-ss <swadeshishu2001@gmail.com>
2022-01-10 17:19:40 +0530

    tired

--- src/App.js
+++ src/App.js
@@ -1,12 +1,115 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
+  <>
     <div className="App">
-      
+      <header>
+
+        <div className="topnav" id="myTopnav">
+          <div id="logo2">&copy;PSPORTS </div>
+          {/* <a href="clothing.html" className="current">Home</a> */}
+          <a className="dropdown" href="brands.html">Brands</a>
+          {/* <a href="contact.html">Contact Us</a> */}
+          {/* <a href="about.html">About</a> */}
+          {/* <a href="register.html">Register Here</a> */}
     </div>
-  );
+      </header>
+    <div id="button0">
+      {/* <a href="login.html" className="btn blue">Log In</a> */}
+      {/* <a href="register.html" className="btn green">Sign Up</a> */}
+    </div>
+    <body>
+
+      <div className="h1">
+        <h1> <marquee WIDTH="400" HEIGHT="50" BEHAVIOR="ALTERNATE">NEW ARRIVALS!! </marquee> </h1>
+      <figure>
+        <form action="buy.html">
+          <input type="image" src="images/shoes.jpg" alt="Submit"/>
+            <p> <b> NIKE Brands Shoes [MALE | FEMALE] </b></p> 
+
+        </form>
+        <div className="h2">
+          <form action="buysummer.html">
+            <br> <h2> <b> Stand out this summer in florals, stripes and colors that pop with our favorite new looks. </b> </h2> </br> 
+          <input type="image" src="images/summerlooks.jpg" alt="Submit" title="Click For More Details">
+            <input type="image" src="images/summerlooks2.jpg" alt="Submit" title="Click For More Details"/>
+              <input type="image" src="images/summerlooks3.jpg" alt="Submit" title="Click For More Details">
+                <h3> <b> Summer Looks For Men! </b> </h3>
+                <input type="image" src="images/msummerlooks.jpg" alt="Submit" title="Click For More Details"/>
+                  <input type="image" src="images/msummerlooks2.jpg" alt="Submit" title="Click For More Details"/>
+                    <input type="image" src="images/msummerlooks3.jpg" alt="Submit" title="Click For More Details"/>
+                    </form>
+                  </div>
+                </div>
+                <div>
+                  <img id="topimg" src="images/slider/rbags2.jpg" />
+                </div>
+              </body>
+              <b><p>  Rebok Bags </p> </b>
+          </form>
+        </div>
+        <div className="h3">
+          <h3> <br> Grab Branded Perfumes. Click The Picture Below  </br></h3>
+            <form action="perfume.html">
+              <input type="image" src="images/perfume.jpg" alt="Submit" title="Click For More Details"/>
+                <input type="image" src="images/perfume2.jpg" alt="Submit" title="Click For More Details"/>
+                  <input type="image" src="images/perfume3.jpg" alt="Submit" title="Click For More Details"/>
+                    <input type="image" src="images/perfume4.jpg" alt="Submit" title="Click For More Details"/>
+                    </form>
+                    </div>
+                  
+
+                  <div className="h4">
+                    <h4> <br> <marquee WIDTH="400" BEHAVIOR="ALTERNATE"> FOOTBALL SHOES!</marquee> </br> </h4> 
+                  <figure>
+                    <form action="boots.html">
+                      <input type="image" src="images/adidasmessi.jpg" alt="Submit" title="Click Here To Shop Now!"/>
+                      </form>
+                       </figure>
+                      <div/> 
+
+        </figure>
+                <h5> JORDAN BRANDS </h5>
+                <video width="1200" height="300" controls autoplay loop>
+                  <source src="video/Jordan Brand.mp4" type="video/mp4"/>
+                    <source src="video/Jordan Brand.ogg" type="video/ogg"/>
+                      Your browser does not support HTML5 video.
+                    <video/>
+
+                    <a href="jordan.html" className="button12">Shop Now</a>
+
+                    <form action="jordan.html">
+                      <input type="image" src="images/jordanbrand.jpg" alt="Submit" title="Click Here To Shop Now!"/>
+                      <div/> <form/>
+
+                    <h6>  ZARA - DIESEL CLOTHING <br> <i> ADVERTISEMENTS </i> </br>  </h6>
+                  <div className="btn1"><form>
+                    <input type="button" value="Visit ZARA" onclick="window.location.href='http://www.zara.com'" />
+                  </form> </div>
+
+                  <div className="btn2">
+                    <form>
+                    <input type="button" value="Visit DIESEL CLOTHING" onclick="window.location.href='http://global.diesel.com/homepage?origin=NOUS'" />
+                  </form> </div>
+
+                  <video width="600" height="300" controls autoplay loop>
+                    <source src="./src/video/Zara man.mp4" type="video/mp4"/>
+                      <source src="video/Zara man.ogg" type="video/ogg"/>
+                        Your browser does not support HTML5 video.
+                      </video>
+
+                      <video width="600" height="300" controls autoplay loop>
+                        <source src="video/Diesel clothing.mp4" type="video/mp4"/>
+                          <source src="video/Diesel clothing.ogg" type="video/ogg"/>
+
+                            Your browser does not support HTML5 video.
+                          </video>
+
+                        </div>
+                      </>
+                      );
 }
 
-export default App;
+                      export default App;
